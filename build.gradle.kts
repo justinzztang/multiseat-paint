5a4f3c775a1cecafbf06ad2b39aae7b81bd1d1f0
justinzztang <justinzztang@gmail.com>
2026-08-09 20:15:33 -0400

    Add Spring Boot stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,16 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "4.1.0"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
 
 repositories {
     mavenCentral()
@@ -12,6 +20,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 }
 
 tasks.test {
