9660f6fdb535a9de00c11218fa7d3593668d98d8
justinzztang <justinzztang@gmail.com>
2026-08-10 19:09:12 -0400

    add spring boot websocket dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+
 }
 
 tasks.test {
